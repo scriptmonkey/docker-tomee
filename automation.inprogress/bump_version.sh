fef68d4dcf8122dc9bebeb38beeade7dbfe6b890
Rod Jenkins <jenkir14@nationwide.com>
2021-09-25 02:07:48 -0500

    added script to bump versions

--- automation.inprogress/bump_version.sh
+++ automation.inprogress/bump_version.sh
@@ -2,14 +2,35 @@
 
 NEW_VERSION="$1"
 
-KEY_SECTION=`./create_key_section.sh`
+MAJOR_VER=`echo $NEW_VERSION | cut -d . -f1`
+MINOR_VER=`echo $NEW_VERSION | cut -d . -f2`
+MCIRO_VER=`echo $NEW_VERSION | cut -d . -f3`
 
-flavors=(webprofile plus plume microprofile)
+KEY_SECTION=`./automation.inprogress/create_key_section.sh`
+
+BUILD_SECTION=`cat ./automation.inprogress/build.template`
+
+FLAVORS=(webprofile plus plume microprofile)
 
 
-
-for flavor in ${flavors[*]}
+for file in `find ./TomEE-$MAJOR_VER.$MINOR_VER -name Dockerfile`
 do
-  echo "$NEW_VERSION"
-  echo "$KEY_SECTION"
+  JAVA_VER=`echo $file | cut -d / -f3 | grep -o -E "[0-9]+"`
+
+  for flavor in ${FLAVORS[*]}
+  do
+    echo "FROM openjdk:11-jre" > $file
+    echo "" >> $file
+    echo "ENV PATH /usr/local/tomee/bin:\$PATH" >> $file
+    echo "RUN mkdir -p /usr/local/tomee">> $file
+    echo "">> $file
+    echo "WORKDIR /usr/local/tomee" >> $file
+    echo "" >> $file
+    echo "$KEY_SECTION" >> $file
+    echo "" >> $file
+    echo "ENV TOMEE_VER $NEW_VERSION" >> $file
+    echo "ENV TOMEE_BUILD $flavor" >> $file
+    echo "" >> $file
+    echo "$BUILD_SECTION" >> $file
+  done
 done
--- automation.inprogress/create_key_section.sh
+++ automation.inprogress/create_key_section.sh
@@ -3,7 +3,8 @@
 
 echo "RUN set -xe; \\"
 echo "  for key in \\"
-docker run --rm buildpack-deps:bullseye-curl bash -c 'curl -fsSL https://www.apache.org/dist/tomee/KEYS | gpg --batch --import &> /dev/null && gpg --batch --list-keys --with-fingerprint --with-colons' |\
+#docker run --rm buildpack-deps:bullseye-curl bash -c 'curl -fsSL https://www.apache.org/dist/tomee/KEYS | gpg --batch --import &> /dev/null && gpg --batch --list-keys --with-fingerprint --with-colons' |\
+curl -fsSL https://www.apache.org/dist/tomee/KEYS | gpg --batch --import &> /dev/null && gpg --batch --list-keys --with-fingerprint --with-colons |\
   awk -F: '$1 == "pub" && $2 == "-" { pub = 1 } pub && $1 == "fpr" { fpr = $10 } $1 == "sub" { pub = 0 } pub && fpr && $1 == "uid" && $2 == "-" { print "  #", $10; print "  " fpr " \\"; pub = 0 }'
 echo "  ; do \\"
 echo "    gpg --batch --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys \"\$key\" || \\"
